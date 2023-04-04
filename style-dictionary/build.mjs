e879e29e9187a08a08eae202befc1754769a9e3f
ltwmori <ltwmori@gmail.com>
2023-04-04 23:55:25 +0600

    feat: built scss variables

--- style-dictionary/build.mjs
+++
@@ -1,5 +0,0 @@
-import StyleDictionaryPackage from 'style-dictionary';
-import './svelte-transforms.mjs';
-
-const { extend } = StyleDictionaryPackage;
-extend(new URL('./config.json', import.meta.url)).buildAllPlatforms();
